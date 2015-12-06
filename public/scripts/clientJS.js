a170ac2b91c69e2a509c190d3bd0d54c8999b043
amit <amitk26467@gmail.com>
2015-12-06 13:29:28 -0500

    https socket issue

--- public/scripts/clientJS.js
+++ public/scripts/clientJS.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://localhost:5000');
+var socket = io.connect('http://localhost:5000',{secure: true});
 var user;
 $(document).ready(function(){
 	// SUBMIT A CHAT MESSAGE
